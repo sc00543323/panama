07e20ddad5ff4ff1794bd1ece4104d0ab2e25661
Uthayaselvan Ramar <UR00477695@techmahindra.com>
2018-04-18 20:34:12 +0530

    prepaid journey1

--- app/code/Panama/Port/Controller/Index/Index.php
+++ app/code/Panama/Port/Controller/Index/Index.php
@@ -5,15 +5,21 @@ use Magento\Framework\Controller\ResultFactory;
 
 class Index extends \Magento\Framework\App\Action\Action {
 
-	protected $_url; 
+	protected $_storeManager;
+	protected $_categoryRepository;
+	protected $_url;
 	protected $_checkoutSession;
 	
     public function __construct(
 		\Magento\Framework\App\Action\Context $context,
+		\Magento\Store\Model\StoreManagerInterface $storeManager,
+		\Magento\Catalog\Api\CategoryRepositoryInterface $categoryRepository,
 		\Magento\Framework\UrlInterface $url,
 		\Magento\Checkout\Model\Session $checkoutSession
 	){
 		parent::__construct($context);
+		$this->_storeManager = $storeManager;
+        $this->_categoryRepository = $categoryRepository;
 		$this->_url = $url;
 		$this->_checkoutSession = $checkoutSession;
 	}
@@ -28,11 +34,28 @@ class Index extends \Magento\Framework\App\Action\Action {
 			$this->_checkoutSession->setCurrentService($data['current_service']);
 			$this->_checkoutSession->setBuySmartphone($data['buy_smartphone']);
 			$resultJson = $this->resultFactory->create(ResultFactory::TYPE_JSON);
+			if($data['current_service'] == 'postpaid') {
+				$categoryId = 7;
+			} else if($data['current_service'] == 'prepaid') {
+				$categoryId = 6;
+			} else {
+				$categoryId = '';
+			}
 			if($data['port'] == 'no') {
 				$resultJson->setData($this->_url->getUrl('customcatalog/cart/add/', array('id' => $data['product_id'])));
-				return $resultJson; die;
+				
+			} else if($data['current_service'] == 'postpaid' || $data['current_service'] == 'prepaid') {
+				$category = $this->_categoryRepository->get($categoryId, $this->_storeManager->getStore()->getId());
+				$resultJson->setData($category->getUrl());
+			} else if($data['buy_smartphone'] == 'no') {
+				$resultJson->setData($this->_url->getUrl('customcatalog/cart/add/', array('id' => $data['product_id'])));
+			} else if($data['buy_smartphone'] == 'yes') {
+				$categoryId = 4;
+				$category = $this->_categoryRepository->get($categoryId, $this->_storeManager->getStore()->getId());
+				$resultJson->setData($category->getUrl());
+			} else {
+				$resultJson->setData($data);
 			}
-			$resultJson->setData($data);
 			return $resultJson; die;
         } else {
             $model = __('This is Not An Ajax Call');
