97f7e77afbd12a623f6a7db583bb91835a0004a8
Uthayaselvan Ramar <UR00477695@techmahindra.com>
2018-04-25 16:02:06 +0530

    prepaid journey 6

--- app/code/Panama/Port/Controller/Index/Index.php
+++ app/code/Panama/Port/Controller/Index/Index.php
@@ -8,33 +8,59 @@ class Index extends \Magento\Framework\App\Action\Action {
 	protected $_storeManager;
 	protected $_categoryRepository;
 	protected $_url;
-	protected $_checkoutSession;
+	protected $_catalogSession;
+	protected $cart;
 	
     public function __construct(
 		\Magento\Framework\App\Action\Context $context,
 		\Magento\Store\Model\StoreManagerInterface $storeManager,
 		\Magento\Catalog\Api\CategoryRepositoryInterface $categoryRepository,
 		\Magento\Framework\UrlInterface $url,
-		\Magento\Catalog\Model\Session $checkoutSession
+		\Magento\Catalog\Model\Session $catalogSession,
+		\Magento\Checkout\Model\Cart $cart
 	){
 		parent::__construct($context);
 		$this->_storeManager = $storeManager;
         $this->_categoryRepository = $categoryRepository;
 		$this->_url = $url;
-		$this->_checkoutSession = $checkoutSession;
+		$this->_catalogSession = $catalogSession;
+		$this->cart = $cart;
 	}
 
     public function execute() {
+			//prepaid and postpaid addtocart popup ajax calling
         if ($this->getRequest()->isAjax()) {
 			$data['product_id'] = $this->getRequest()->getParam('product_id');
 			$data['port'] = $this->getRequest()->getParam('port');
 			$data['current_service'] = $this->getRequest()->getParam('current_service');
 			$data['buy_smartphone'] = $this->getRequest()->getParam('buy_smartphone');
-			$this->_checkoutSession->setPort($data['port']);
-			$this->_checkoutSession->setCurrentService($data['current_service']);
-			$this->_checkoutSession->setBuySmartphone($data['buy_smartphone']);
+			$data['port_remove'] = $this->getRequest()->getParam('port_remove');
+			if($data['port_remove'] == 'no') {
+				$data['port'] = 'no_'.$data['product_id'];
+			}
+			//$this->_catalogSession->setPortProductId($data['product_id']);
+			$this->_catalogSession->setPort($data['port']);
+			$this->_catalogSession->setCurrentService($data['current_service']);
+			$this->_catalogSession->setBuySmartphone($data['buy_smartphone']);
 			$resultJson = $this->resultFactory->create(ResultFactory::TYPE_JSON);
-			if($data['buy_smartphone'] == 'no') {
+			$dataPort = explode('_',$data['port']);
+			if($dataPort[0] == 'yes' && !$data['current_service'] && !$data['buy_smartphone']) {
+				$itemCount = $this->cart->getQuote()->getItemsCount();
+				if ($itemCount == 0 ) {
+					$resultJson->setData(true);
+				} else if($itemCount > 0) {
+					$allItems = $this->cart->getQuote()->getAllItems();
+					$portability = true;
+					foreach($allItems as $item) {
+						$portable = explode('_',$item->getIsPortable());
+						if($portable[0] == 'yes') {
+							$portability = false;
+							break;
+						}
+					}
+					$resultJson->setData($portability);
+				}
+			} else if($data['buy_smartphone'] == 'no') {
 				$resultJson->setData($this->_url->getUrl('customcatalog/cart/add/', array('id' => $data['product_id'])));
 			} else if($data['buy_smartphone'] == 'yes') {
 				$categoryId = 4;
