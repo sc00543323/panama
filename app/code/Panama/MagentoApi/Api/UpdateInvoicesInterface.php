ca2c10273173fa06f54ccbcd5e3b86d8d9e9103b
Uthayaselvan Ramar <UR00477695@techmahindra.com>
2018-05-14 12:55:56 +0530

    implemented UpdateInvoices Api

---
+++ app/code/Panama/MagentoApi/Api/UpdateInvoicesInterface.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Copyright 2015 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Panama\MagentoApi\Api;
+/**
+ * Defines the service contract for some simple maths functions. The purpose is
+ * to demonstrate the definition of a simple web service, not that these
+ * functions are really useful in practice. The function prototypes were therefore
+ * selected to demonstrate different parameter and return values, not as a good
+ * calculator design.
+ */
+interface UpdateInvoicesInterface
+{
+   /**
+     * @param \Panama\MagentoApi\Api\Data\InvoicesInterface $invoicesData
+     * @return \Panama\MagentoApi\Api\Data\InvoicesInterface
+     */
+    public function updateInvoices(\Panama\MagentoApi\Api\Data\InvoicesInterface $invoicesData);
+}
