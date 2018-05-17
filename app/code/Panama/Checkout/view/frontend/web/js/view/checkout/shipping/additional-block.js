317e6342bafd83141de42fb37cf408c288458820
Uthayaselvan Ramar <UR00477695@techmahindra.com>
2018-05-17 15:27:12 +0530

    calculate api with sample code

--- app/code/Panama/Checkout/view/frontend/web/js/view/checkout/shipping/additional-block.js
+++ app/code/Panama/Checkout/view/frontend/web/js/view/checkout/shipping/additional-block.js
@@ -12,11 +12,11 @@ define([
         },
 		
 		selectButton: function () {
+			var dateTimeObservableArray = ko.observableArray();
             var city = $("[name='city']").val();
 			var address = $("[name='street[0]']").val();
-			//alert(city);
-			//alert(address);
 			var checkout = window.checkout;
+			dateTimeObservableArray.push.apply(dateTimeObservableArray, [{ delivery_time: "12", start_time: "23" }]);
 			$.ajax({
 				url: checkout.baseUrl+'checkout/calculate/delivery',
 				dataType: 'json',
@@ -24,14 +24,25 @@ define([
 				type: 'post',
 				data: city,
 				success: function (data) {
-					if(data == true) {
-						//alert("succsesss");
+					var result_id = data[0].ResultId;
+					var result_message = data[0].ResultMessage;
+					var delivery_array = data[0].DeliveryDateRangeList;
+					if(result_id == 1) {
+						var append_html ='';
+						for (var i=0;i<delivery_array.length;++i) {
+							dateTimeObservableArray.push.apply(dateTimeObservableArray, [{ delivery_time: delivery_array[i]._jornada, start_time: delivery_array[i]._horaInicioEntrega }]);
+							append_html += '<input type="radio" data-bind="click: time" name="time" value="'+delivery_array[i]._jornada+'">'+delivery_array[i]._jornada+'<br>';
+						}
+						/*alert('The length of the array is ' + dateTimeObservableArray().length);
+						alert('The first element is ' + dateTimeObservableArray()[0]['delivery_time']);
+						alert('The first element is ' + dateTimeObservableArray()[1]['delivery_time']);*/
+						$(".time_div").html(append_html);
 					} else {
-						//alert("faiii");
+						$(".time_div").html("somthing went wrong!");
 					}
 				}
 			});
-            //$(".time_date_div").toggle();
+            $(".time_date_div").toggle();
         },
 		
 		time: function () {
@@ -39,7 +50,6 @@ define([
 			var date = $(".date_div p").html();
 			$("#delivery_date_time").val(date+' '+time);			
 			$("[name='custom_attributes[delivery_date_time]']").val(date+' '+time);
-        }
-		
+        }		
     });
 });
