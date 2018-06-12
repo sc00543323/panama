76286756644a3ba99715b70ad1ad169ff1063360
Uthayaselvan Ramar <UR00477695@techmahindra.com>
2018-06-12 14:31:17 +0530

    log enable code

--- app/code/Panama/MagentoApi/Helper/Data.php
+++ app/code/Panama/MagentoApi/Helper/Data.php
@@ -19,9 +19,12 @@ class Data extends \Magento\Framework\App\Helper\AbstractHelper {
     }
 
     public function logCreate($fileName, $data) {
-		$writer = new \Zend\Log\Writer\Stream(BP . "$fileName");
-		$logger = new \Zend\Log\Logger();
-		$logger->addWriter($writer);
-		$logger->info($data);
+		$isEnable = $this->_scopeConfig->getValue('panama/log_api/save_response', \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
+		if($isEnable) {
+			$writer = new \Zend\Log\Writer\Stream(BP . "$fileName");
+			$logger = new \Zend\Log\Logger();
+			$logger->addWriter($writer);
+			$logger->info($data);
+		}
     }
 }
